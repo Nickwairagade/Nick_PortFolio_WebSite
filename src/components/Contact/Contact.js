6879b4b2a4f9a78603ed343ecc4ab5e12226207c
Nickwairagade <nickwairagade25@gmail.com>
2022-07-14 19:39:58 +0530

    contact and profile page changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Style.css'
-import me from "../../assets/images/contact-01.webp";
-import HomeIcon from '@material-ui/icons/Home';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import MailIcon from '@material-ui/icons/Mail';
+// import me from "../../assets/images/contact-01.webp";
+// import HomeIcon from '@material-ui/icons/Home';
+// import ContactsIcon from '@material-ui/icons/Contacts';
+// import MailIcon from '@material-ui/icons/Mail';
 
 
 
@@ -11,35 +11,56 @@ function Contact() {
     return (
         <>
             <div className='container'>
-                <div className='row contact'>
-                    <div className='col-md-7 col-sm-12'>
-                        <h4>Contact Us</h4>
-                        <div className='row'>
-                            <div className='col-1'>
-                                <HomeIcon />
+                <div class="background">
+                    <div class="container">
+                        <div class="screen">
+                            <div class="screen-header">
+                                <div class="screen-header-left">
+                                    <div class="screen-header-button close"></div>
+                                    <div class="screen-header-button maximize"></div>
+                                    <div class="screen-header-button minimize"></div>
+                                </div>
+                                <div class="screen-header-right">
+                                    <div class="screen-header-ellipsis"></div>
+                                    <div class="screen-header-ellipsis"></div>
+                                    <div class="screen-header-ellipsis"></div>
+                                </div>
                             </div>
-                            <div className='col-11'>
-                                <span><p>Narendra Nagar Nagpur</p></span>
+                            <div class="screen-body">
+                                <div class="screen-body-item left">
+                                    <div class="app-title">
+                                        <span>CONTACT</span>
+                                        <span>US</span>
+                                    </div>
+                                    <div class="app-contact">CONTACT INFO : 9860489971</div>
+                                </div>
+                                <div class="screen-body-item">
+                                    <div class="app-form">
+                                        <div class="app-form-group">
+                                            <input class="app-form-control" placeholder="NAME" value="Nikhil Wairagade" />
+                                        </div>
+                                        <div class="app-form-group">
+                                            <input class="app-form-control" value="nickwairagade@gmail.com" placeholder="EMAIL" />
+                                        </div>
+                                        <div class="app-form-group">
+                                            <input class="app-form-control" value="9860489971" placeholder="CONTACT NO" />
+                                        </div>
+                                        {/* <div class="app-form-group message">
+                                            <input class="app-form-control" placeholder="MESSAGE" />
+                                        </div>
+                                        <div class="app-form-group buttons">
+                                            <button class="app-form-button">CANCEL</button>
+                                            <button class="app-form-button">SEND</button>
+                                        </div> */}
+                                    </div>
+                                </div>
                             </div>
-                            <div className='col-1'>
-                                <ContactsIcon />
-                            </div>
-                            <div className='col-11'>
-                                <p>9998889998</p>
-                            </div>
-                            <div className='col-1'>
-                                <MailIcon />
-                            </div>
-                            <div className='col-11'>nickwairagade@gmail.com</div>
                         </div>
-                    </div>
-                    <div className='col-md-5 col-sm-12'>
-                        <img src={me} alt="" />
                     </div>
                 </div>
             </div>
         </>
-        
+
     )
 }
 
