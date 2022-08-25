ea3f2944e15b98219cdcf7af6f6088d817e03330
Nickwairagade <nickwairagade25@gmail.com>
2022-08-25 11:35:08 +0530

    chnages in specializing file

--- src/components/UI/SocialConnect/SocialConnect.js
+++ src/components/UI/SocialConnect/SocialConnect.js
@@ -1,12 +1,10 @@
 import React from "react";
-import socialIcons from "../../../assets/social-icons";
+// import socialIcons from "../../../assets/social-icons";
 import "./Style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram
+  faGit,
+  faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 
 /**
@@ -17,35 +15,17 @@ import {
 const SocialConnect = (props) => {
   return (
     <div className="socialConnect" style={props.style}>
-      <span className="textColor font-12">Follow me :  </span>
+      <span className="textColor font-12">Follow me : </span>
       <div class="social-container">
-          <a href=""
-            className="youtube social">
-            <FontAwesomeIcon icon={faYoutube} size="2x"/>
+        <div className="git">
+          <a href="https://github.com/Nickwairagade" className="git social">
+            <FontAwesomeIcon icon={faGit} size="2x" />
           </a>
-          <a href=""
-            className="facebook social">
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          <a href="" className="linkedin social">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
-          <a href="" className="twitter social">
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
-          </a>
-          <a href="" className="instagram social">
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
+        </div>
       </div>
-      {/* <a className="socialLink" href="#">
-        <img src={socialIcons.facebook} alt="" />
-      </a>
-      <a className="socialLink" href="#">
-        <img src={socialIcons.instagram} alt="" />
-      </a>
-      <a className="socialLink" href="#">
-        <img src={socialIcons.youtube} alt="" />
-      </a>
-      <a className="socialLink" href="#">
-        <img src={socialIcons.linkedin} alt="" />
-      </a> */}
     </div>
   );
 };
